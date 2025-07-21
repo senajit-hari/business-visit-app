ece65c07886a194236eff1096bc53232baf6a1b9
Senajit Hari <senajithari@gmail.com>
2025-07-21 18:51:35 +0300

    Fix port binding for Render deployment

--- web_business_visit.py
+++ web_business_visit.py
@@ -251,4 +251,9 @@ if __name__ == '__main__':
     app.run(debug=True)
 if __name__ == '__main__':
     app.run()
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Get PORT from env or default 5000 locally
+    app.run(host='0.0.0.0', port=port)
 
